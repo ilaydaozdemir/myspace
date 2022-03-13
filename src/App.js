dbf872aeb7ec3b1c66e6dfcea66efd2e6791c616
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-03-13 22:49:40 +0300

    Use Arrow function

--- src/App.js
+++ src/App.js
@@ -2,25 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const name="improve my react skills"
+  //use Arrow function
+  //use random function
+  const handleNameChange = () => {
+    const names = ['orange', 'apple', 'pink'];
+    const int = Math.floor(Math.random() * 3);
+    return names[int];
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {handleNameChange()}!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      <p>{name}</p>
       </header>
-     
+
     </div>
   );
 }
