8595562f74e791a869baaaa697096065b8b359ce
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-03-13 23:27:25 +0300

    Applying css style

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,8 +3,13 @@
 import React from "react";
 
 const Header = () => {
+  //add style
+  const headerStyle = {
+    backgroundColor: "mediumblue",
+    color: "#fff",
+  };
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>Groceries List</h1>
     </header>
   );
