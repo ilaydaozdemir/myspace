31edc20cf2d4b61eec9f69532ba7e0fc871fe878
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-03-14 16:08:54 +0300

    Click Events

--- src/component/Content.jsx
+++ src/component/Content.jsx
@@ -8,9 +8,13 @@ const Content = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+  const handleClick = () => {
+    console.log("clicked");
+  };
   return (
     <main>
       <p>Hello {handleNameChange()}</p>
+      <button onClick={handleClick}>START</button>
     </main>
   );
 };
