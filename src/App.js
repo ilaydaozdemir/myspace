31452c46ea70e8d69d407e308202797c428b3b0f
ilaydaozdemir <developerilaydaozdemir@gmail.com>
2022-03-16 18:16:09 +0300

    Add SeachItem filter

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
         setSearch={setSearch}
       />
       <Content
-        items={items}
+        items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
       />
